05567e0b8cfa0afd372005f3df38c4554793ae24
Mohit <mohit2412gupta@gmail.com>
2023-12-23 01:25:18 +0530

    API endpoint for getting prompts

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,7 +5,7 @@ import Prompt from '@models/prompt'
 export const GET = async (request)=>{
     try {
         await connectToDB()
-        const prompts = await Prompt.find({}).populate('creator');
+        const prompts = await Prompt.find({}).populate('creator').maxTime(30000);
         return new Response(JSON.stringify(prompts),{
             status: 200,
         })
